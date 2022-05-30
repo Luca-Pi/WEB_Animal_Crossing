732aaa67683d4860f96760638c998aefb065c227
LoganLS <logan_56@hotmail.fr>
2022-05-30 09:29:50 +0200

    fix(logout): remove locale storage even if token is expired

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -91,8 +91,6 @@ export const useUserStore = defineStore("userStore", () => {
         )
       ).data;
 
-      if (!success) return reject(message);
-
       user.value = {};
 
       return resolve(true);
