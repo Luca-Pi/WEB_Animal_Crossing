3790dd8b43daaadeec1cfaa04f9d3cf09a12e3d1
BeatlesBOB <beatlesbob74120@gmail.com>
2022-05-30 17:36:03 +0200

    feat(header) : style header

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -1,65 +1,44 @@
 <template>
-  <q-header class="bg-secondary" elevated reveal>
+  <q-header style="background-color:#9CCFC6;" elevated reveal>
     <q-toolbar class="row full-height">
-      <div class="col-3 text-left">
-        <q-btn :to="{ name: 'blog' }" color="secondary" unelevated>Blog</q-btn>
-      </div>
-
-      <div class="col-6 text-center">
+      <div class="col-5 ">
         <RouterLink to="/" custom v-slot="{ navigate }">
           <div @click="navigate" class="cursor-pointer">
             <q-avatar>
               <img
-                src="https://cdn.pixabay.com/photo/2020/07/27/10/33/animal-crossing-leaf-5442035_1280.png"
+                src="/images/svg/icon.svg"
+                style="
+                  margin-left: 0;
+                  margin-right: 0;
+                  width: 30px;
+                "
               />
             </q-avatar>
-            <span class="text-h5 q-mx-lg">Island Crossing</span>
-            <q-avatar>
-              <img
-                src="https://cdn.pixabay.com/photo/2020/07/27/10/33/animal-crossing-leaf-5442035_1280.png"
-              />
-            </q-avatar>
+            <span
+              class="text-h5"
+              style="font-size: medium; margin-left: 0; margin-right: 0"
+              >Island Crossing</span
+            >
           </div>
         </RouterLink>
       </div>
 
-      <div class="col-3 text-right">
-        <audio src="../src/assets/audios/theme.mp3" loop></audio>
-        <q-btn
-          @click="toggleAudio()"
-          icon="fab fa-soundcloud"
-          :class="audio_playing ? 'icon-sound' : 'icon-sound icon-sound-mute'"
-          round
-          flat
-        />
-        <template v-if="userStore.user.id">
-          <q-btn-dropdown
-            :label="userStore.user.username"
-            color="secondary"
-            unelevated
-          >
-            <q-list>
-              <q-item :to="{ name: 'profile' }" clickable v-close-popup>
-                <q-item-section>
-                  <q-item-label>Mon profil</q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-item @click="logout" clickable v-close-popup>
-                <q-item-section>
-                  <q-item-label>Déconnexion</q-item-label>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-btn-dropdown>
-        </template>
-        <template v-else>
-          <q-btn :to="{ name: 'login' }" color="secondary" unelevated
-            >Connexion</q-btn
-          >
-          <q-btn :to="{ name: 'register' }" color="secondary" unelevated
-            >Inscription</q-btn
-          >
-        </template>
+      <div class="col text-center" >
+        <span
+          class="text-h5"
+          style="font-size: medium;margin: 2.5%;"
+          >Profil</span
+        >
+        <span
+          class="text-h5"
+          style="font-size: medium; margin: 2.5%;"
+          >Catégorie</span
+        >
+        <span
+          class="text-h5"
+          style="font-size: medium;margin: 2.5%;"
+          >Anniversaire</span
+        >
       </div>
     </q-toolbar>
     <link
@@ -72,46 +51,7 @@
   </q-header>
 </template>
 
-<script setup lang="ts">
-import { ref } from "vue";
-import router from "@/router";
-import { RouterLink } from "vue-router";
 
-import { useQuasar } from "quasar";
-import { useUserStore } from "@/stores/user";
-
-const $q = useQuasar();
-const userStore = useUserStore();
-
-const audio_playing = ref(false);
-
-async function logout() {
-  try {
-    $q.notify({
-      message: `À très bientôt ${userStore.user.username} !`,
-      type: "positive",
-    });
-
-    await userStore.logout();
-    router.push({ name: "login" });
-  } catch (error: any) {
-    $q.notify({
-      message: error || "Une erreur est survenu lors de la déconnexion.",
-      type: "negative",
-    });
-  }
-}
-
-function toggleAudio() {
-  const audio = document.getElementsByTagName("audio")[0];
-  if (audio.paused) {
-    audio.play();
-  } else {
-    audio.pause();
-  }
-  audio_playing.value = !audio.paused;
-}
-</script>
 
 <style scoped lang="scss">
 .q-header {
