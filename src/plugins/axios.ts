c752971e19f8c6bc0d8b532b2436e183a7d0aac4
LoganLS <logan_56@hotmail.fr>
2022-05-30 16:43:54 +0200

    feat(footer): refonte footer + cablage back axios

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -4,6 +4,8 @@ const http = axios.create();
 
 if (import.meta.env.PROD) {
   http.defaults.baseURL = "https://islandcrossing.anaelbonnafous.fr";
+} else {
+  http.defaults.baseURL = 'http://localhost:8000';
 }
 
 export default http;
