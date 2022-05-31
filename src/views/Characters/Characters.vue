a2989615531d4d3c099c8749bb6f2b4a8d0f76e4
LoganLS <logan_56@hotmail.fr>
2022-05-31 11:05:35 +0200

    feat(characters): refonte

--- src/views/Characters/Characters.vue
+++ src/views/Characters/Characters.vue
@@ -1,38 +1,38 @@
 <template>
-  <q-page padding class="text-center">
-    <q-card>
-      <q-card-section>
+  <q-page class="text-center">
+    <section class="listing-bulle-header">
+      <Bubble>
         <h2>Les personnages des Jeux Animal Crossing</h2>
-      </q-card-section>
-      <q-card-section>
         <p>
           Cette catégorie regroupe tous les personnages qui ne sont pas
           villageois à part entière et qui ont un rôle particulier.
         </p>
-      </q-card-section>
-    </q-card>
-
-    <section id="section-list">
-      <CharacterCard
-        v-for="character in charactersFiltered"
-        :key="character.id"
-        :id="character.id"
-        :name="character.name"
-        :image="character.photoImage"
-      />
+      </Bubble>
     </section>
 
-    <div class="q-pa-lg flex flex-center">
-      <q-pagination
-        v-if="characters.length > maxItemsPerPage"
-        v-model="currentPage"
-        color="#000"
-        active-text-color="#000"
-        active-color="secondary"
-        :max="Math.ceil(characters.length/maxItemsPerPage)"
-        :max-pages="10"
-      />
-    </div>
+    <Cliff>
+      <section id="section-list">
+        <CharacterCard
+          v-for="character in charactersFiltered"
+          :key="character.id"
+          :id="character.id"
+          :name="character.name"
+          :image="character.photoImage"
+        />
+      </section>
+
+      <div class="q-pa-lg flex flex-center">
+        <q-pagination
+          v-if="characters.length > maxItemsPerPage"
+          v-model="currentPage"
+          color="#000"
+          active-text-color="#000"
+          active-color="secondary"
+          :max="Math.ceil(characters.length/maxItemsPerPage)"
+          :max-pages="10"
+        />
+      </div>
+    </Cliff>
   </q-page>
 </template>
 
@@ -40,6 +40,8 @@
 import { onBeforeMount, ref, computed } from "vue";
 
 import CharacterCard from "@/components/Cards/Character.vue";
+import Bubble from "@/components/Bubble.vue";
+import Cliff from "@/components/Cliff.vue";
 
 import { useQuasar } from "quasar";
 import { useCharactersStore, type Character } from "@/stores/characters";
